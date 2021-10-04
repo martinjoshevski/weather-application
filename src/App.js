c8c7cec8ceb9250f8d382fb3912811fe41bf3a95
Martin <61944410+martinjoshevski@users.noreply.github.com>
2021-10-04 16:33:28 +0200

    Adding datebuilder

--- src/App.js
+++ src/App.js
@@ -4,13 +4,50 @@ const api = {
   key: `${process.env.REACT_APP_KEY}`,
   base: "https://api.openweathermap.org/data/2.5",
 };
-
+const dateBuilder = (n) => {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuseday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[n.getDay()];
+  let date = n.getDate();
+  let month = months[n.getMonth()];
+  let year = n.getFullYear();
+  return `${day} ${date} ${month} ${year}`;
+};
 function App() {
   return (
     <div className="app">
       <main>
         <div className="search__box">
           <input type="text" className="search__bar" placeholder="Search..." />
+        </div>
+        <div className="location__box">
+          <div className="location">London, UK</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather__box">
+          <div className="temp">15&deg;c</div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
